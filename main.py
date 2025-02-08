3f23720497d0b3b5867591acf9456734f192ec15
Neha Kotturu <nekotturu@gmail.com>
2025-02-08 18:28:17 -0500

    evil hangman game

---
+++ main.py
@@ -0,0 +1,54 @@
+#group the words based on the word families 
+def group_words(words, guess):
+    groups = {}
+    for word in words:
+        #add each word to the group based on the location of the guessed characters
+        pattern = ''.join([char if char in guess else '*' for char in word])
+        if pattern not in groups:
+            groups[pattern] = []
+        groups[pattern].append(word)
+    return groups
+
+#choose the group with the most number of word options
+def choose_largest_group(groups):
+    return max(groups.values(), key=len)
+
+def main():
+    #store all words in an array
+    with open('dictionary.txt', 'r') as file:
+        words = [line.strip() for line in file]
+
+    #parameters to initialize game
+    word_len = int(input("Enter word length: "))
+
+    while(word_len not in [len(word) for word in words]):
+        word_len = int(input("There are no words of that length. Choose a different length: "))
+    
+    tot_guesses = int(input("Enter number of guessses: "))
+
+    curr_num_guesses = 0
+    candidates = [word for word in words if len(word) == word_len]
+    guessed_letters = set()
+    letters_left = list('abcdefghijklmnopqrstuvwxyz')
+    while(curr_num_guesses < tot_guesses):
+        print(' '.join(letters_left))
+        guess = (input("\nGuess a letter: ").lower())
+        while(guess in guessed_letters):
+            guess = (input("You already guessed this letter. Try again: ").lower())
+        guessed_letters.add(guess)
+        curr_num_guesses += 1
+        letters_left.remove(guess)
+        groups = group_words(candidates, guessed_letters)
+        candidates = choose_largest_group(groups)
+        
+        for key, value in groups.items():
+            if value == candidates:
+                guessed_word = key     
+        print(f"\n {guessed_word}\n")
+        
+        if(len(candidates) == 1 and '*' not in guessed_word):
+            print(f"You win!")
+            return
+    print(f"You lose... the word was {candidates[0]}")
+
+main()
